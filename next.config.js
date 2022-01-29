e124b2dd1999ca299a15f0645a11834a6b1d8dd4
Blueskunk22 <jakob.hauble@gmail.com>
2022-01-29 00:10:50 -0600

    PLEASE fren

--- next.config.js
+++ next.config.js
@@ -53,14 +53,6 @@ const nextConfig = {
     }
     return config;
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://2nft.keystone.codes/:path*',
-      },
-    ]
-  },
 };
 
 module.exports = withPlugins(plugins, nextConfig);
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
 }
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
+}
+
+export default handler
