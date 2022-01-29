25ae451debd73bfaff6d31a245fcdf7df2a4b8bd
Blueskunk22 <jakob.hauble@gmail.com>
2022-01-28 21:32:02 -0600

    CORS

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,25 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
 
-export default allowCors(handler)
+export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res)
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
+}
